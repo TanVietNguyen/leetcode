e0bb64498844b599ba76b177da64c2011758fa69
David Nguyen <davidnguyen112@g.ucla.edu>
2026-09-01 16:36:59 -0700

    corrected solution to wordBreak using dynamic programming

--- wordBreak.cpp
+++ wordBreak.cpp
@@ -7,33 +7,63 @@ using namespace std;
 
 class Solution {
 public:
+    // A naive approach is plitting a string in every possible ways, then in each way, determine if every splitted segments of the string are in wordDict.
+    //This approach seem impossible in the first place, but it seems to be feasible with recursive approach, where we just cut the string off one character at a time until it becomes a one-char string. To speed up, use memoization (dynamic programming in other word)
     bool wordBreak(string s, vector<string>& wordDict) {
         int length = s.size();
         if(length == 0){
             return true;
         }
 
-        string temp1, temp2;
-        bool result = false;
-        for(int i = 0; i < length; i++){
-            temp1 = s.substr(0, i + 1);
-            if(find(wordDict.begin(), wordDict.end(), temp1) == wordDict.end()){
-                continue;
-            }
+        vector<vector<int>> result(length + 1, vector<int>(length + 1, -1));
+        return isFound(s, wordDict, result, 0, length - 1);
 
-            temp2 = s.substr(i + 1, length - i);
-            result = result || wordBreak(temp2, wordDict);
-            if(result){
-                return result;
+    }
+
+    bool isFound (string& s, vector<string>& wordDict, vector<vector<int>>& result, int start, int end){
+        if(result[start][end] != -1){
+            return result[start][end];
+        }
+        if(end < start){
+            return true;
+        }
+        else if(start == end){
+            string temp = s.substr(start, 1);
+            if(find(wordDict.begin(), wordDict.end(), temp) != wordDict.end()){
+                result[start][end] = 1;
+                return true;
+            }
+            else{
+                result[start][end] = 0;
+                return false;
             }
         }
-        return false;
-    }
+        else{
+            int length = end - start + 1;
+            string initString;
+            bool found = false;
+            for(int i = start; i < end + 1; i++){
+                initString = s.substr(start, i + 1 - start);
+                if(find(wordDict.begin(), wordDict.end(), initString) == wordDict.end()){
+                    continue;
+                }
+                found = isFound(s, wordDict, result, i + 1, end);
+                if(found){
+                    result[start][end] = 1;
+                    return true;
+                    break;
+                }
+            }
+
+            result[start][end] = 0;
+            return false;
+        }
+    }   
 };
 
 int main (){
     Solution s;
-    string str = "leetcode";
-    vector<string> wordDict = {"leet", "code"};
+    string str = "applepenapple";
+    vector<string> wordDict = {"apple", "pen"};
     cout << s.wordBreak(str, wordDict) << endl;
 }  
